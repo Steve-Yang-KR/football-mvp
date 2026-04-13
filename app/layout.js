7e5db44a2c4b444548ff9d2af33884ca284037ca
Steve-Yang-KR <73861054+Steve-Yang-KR@users.noreply.github.com>
2026-04-13 11:28:22 +0900

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "AI Football App",
+  description: "AI Football Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
